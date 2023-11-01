dfe72184daa0c6a1fbb399495d2c0e29b66a98b9
marketingYouth <youth.marketing.developer@gmail.com>
2023-11-01 11:36:01 +0300

    generate login & customers pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,14 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'customers',
+    loadChildren: () => import('./customers/customers.module').then( m => m.CustomersPageModule)
+  },
 ];
 
 @NgModule({
